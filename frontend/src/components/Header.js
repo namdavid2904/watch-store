29c53ed46a9f887dce7cce5cb3bb4ea82f492401
namdavid2904 <89288986+namdavid2904@users.noreply.github.com>
2025-08-05 23:10:29 -0400

    Add styling for new components

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    <header className="header-top-strip py-3">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-6"></div>
+          <div className="col-6"></div>
+        </div>
+      </div>
+    </header>
+    </>
   )
 }
 
